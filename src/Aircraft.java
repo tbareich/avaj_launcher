80ba90428bc080235a35819d4bb8ad592805df30
tbareich <tareqbareich@gmail.com>
2022-04-18 09:13:57 +0000

    Implement Aircraft class

---
+++ src/Aircraft.java
@@ -0,0 +1,17 @@
+public abstract class Aircraft {
+
+	protected long id;
+	protected String name;
+	protected Coordinates coordinates;
+	private static long idCounter;
+
+	protected Aircraft(String name, Coordinates coordinates) {
+		this.name = name;
+		this.coordinates = coordinates;
+		this.id = nextId();
+	}
+
+	private long nextId() {
+		return (++idCounter);
+	}
+}
