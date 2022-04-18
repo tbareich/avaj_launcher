ec1298ba4fc702a2842dc260cfc2f6d25f2eefaa
tbareich <tareqbareich@gmail.com>
2022-04-18 09:14:31 +0000

    Implement AircraftFactory class

---
+++ src/AircraftFactory.java
@@ -0,0 +1,19 @@
+
+import exceptions.UnkonwnAircraftTypeException;
+
+public abstract class AircraftFactory {
+	public static Flyable newAircraft(String type, String name, int longitude,
+			int latitude, int height)
+			throws UnkonwnAircraftTypeException {
+		if (type == "Baloon")
+			return new Baloon(name, new Coordinates(longitude, latitude,
+					height));
+		else if (type == "JetPlane")
+			return new JetPlane(name, new Coordinates(longitude, latitude,
+					height));
+		else if (type == "Helicopter")
+			return new Helicopter(name, new Coordinates(longitude, latitude,
+					height));
+		throw new UnkonwnAircraftTypeException();
+	}
+}
